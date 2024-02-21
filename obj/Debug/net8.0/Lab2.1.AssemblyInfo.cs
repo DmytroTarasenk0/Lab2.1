e10f4d2f01ff9da234cd7bf372cdc5d7bf288247
Dmytro Tarasenko <dmytro.tarasenko.52@gmail.com>
2024-02-21 11:12:00 +0200

    update 1.

--- obj/Debug/net8.0/Lab2.1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Lab2.1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19d7e696854542384e7bb4c4737a24666d28af55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
